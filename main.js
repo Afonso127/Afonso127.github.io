833846bf1e0a2c8880eab83a7c53fb0dfe963098
Afonso Neves <afonsomaria127@gmail.com>
2023-06-25 10:28:58 +0100

    Added new files

---
+++ main.js
@@ -0,0 +1,20 @@
+function showMail() {
+    var emailBox = document.getElementById("emailBox");
+    if (emailBox.style.display === "none") {
+      emailBox.style.display = "block";
+    } else {
+      emailBox.style.display = "none";
+    }
+  }
+
+  function showTele() {
+    var emailBox = document.getElementById("teleBox");
+    if (emailBox.style.display === "none") {
+      emailBox.style.display = "block";
+    } else {
+      emailBox.style.display = "none";
+    }
+  }
+
+ 
+  
